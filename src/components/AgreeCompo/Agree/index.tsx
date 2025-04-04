e7f98ddd6c35e4de352c90f6e2854f04fb4c702d
heum23 <jinsoon6230@naver.com>
2025-04-04 15:51:02 +0900

    공동이인증서 승인

--- src/components/AgreeCompo/Agree/index.tsx
+++ src/components/AgreeCompo/Agree/index.tsx
@@ -4,12 +4,24 @@ import { useEffect, useState } from "react";
 import { Table, Button } from "antd";
 
 const Agree = () => {
+  const [num, setNum] = useState(0);
+  //승인 상태 변경 함수
+  const pendding = (userId: number) => {
+    axios
+      .post("http://localhost:5000/admins/pendding", { userId })
+      .then((res) => {
+        console.log(res.data);
+        setNum(num + 1);
+      });
+  };
+
   const [file, setFile] = useState([]);
   useEffect(() => {
     axios.get("http://localhost:5000/admins/fileinfo").then((res) => {
+      console.log(res.data);
       setFile(res.data);
     });
-  }, []);
+  }, [num]);
   const dataSource = file
     ? file.map((x: any, i: number) => ({
         key: String(i + 1), // key는 문자열로 변환
@@ -21,7 +33,15 @@ const Agree = () => {
             <Button>파일 보기</Button>
           </a>
         ),
-        agree: <Button>승인</Button>,
+        agree: (
+          <Button
+            onClick={() => {
+              pendding(x.u_id);
+            }}
+          >
+            승인
+          </Button>
+        ),
         refuse: <Button>거절</Button>,
       }))
     : [];
